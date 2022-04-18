6acf2890ba71e1b3306e7b2633f96212c8662601
andrisgribusts <andrisgribusts1@gmail.com>
2022-04-18 13:49:14 +0300

    majasdarbs nr.2

---
+++ mjasdarbs nr.2/mjasdarbs nr.2/Program.cs
@@ -0,0 +1,118 @@
+﻿//See https;//aka.ms/new-console-template for more information
+
+Console.WriteLine("1. uzdevums: Izveidot konsoles aplikāciju, kas pavaicā un  iegūst lietotāja vārdu, un sasveicinās ar konkrēto lietotāju + 2. uzdevums: Atrādīt lietotājam mazāko skaitli izmantojot Math. Min ");
+
+Console.WriteLine("Kā Tevi sauc ?");
+string userName = Console.ReadLine();
+Console.WriteLine("Sveiks: " + userName);
+
+
+Console.WriteLine("Kāds ir Tavs vecums ?");
+string age = Console.ReadLine();
+int ageUser = int.Parse(age);
+int byOneLarger = ageUser + 1;
+bool adultTrue = byOneLarger >= 18;
+bool adultFalse = ageUser >= 18;
+bool adult = adultTrue && adultFalse;
+
+Console.WriteLine("Nākamgad Tev paliks: " + byOneLarger + adult + ", Tu esi pilngadīgs.");
+
+
+Console.WriteLine("3. uzdevums Atrādīt lietotājam lielāko skaitli izmantojot Math. Max ");
+
+Console.WriteLine("Ievadiet pirmo skaitli");
+string firstNumber = Console.ReadLine();
+int firstUserNumber = int.Parse(firstNumber);
+Console.WriteLine("Ievadiet otro skaitli");
+string secondNumber = Console.ReadLine();
+int secondUserNumber = int.Parse(secondNumber);
+int largerNumber = Math.Max(firstUserNumber, secondUserNumber);
+Console.WriteLine("Lielākais no Jūsu ievadītajiem skaitļiem ir: " + largerNumber);
+
+Console.WriteLine("4. uzdevums: Atrādīt lietotājam mazāko skaitli izmantojot Math.Min");
+
+Console.WriteLine("Ievadiet pirmo skaitli");
+string firstNumber1 = Console.ReadLine();
+int firstUserNumber1 = int.Parse(firstNumber1);
+Console.WriteLine("Ievadiet otro skaitli");
+string secondNumber1 = Console.ReadLine();
+int secondUserNumber1 = int.Parse(secondNumber1);
+int smallerNumber = Math.Min(firstUserNumber, secondUserNumber);
+Console.WriteLine("Mazākais no Jūsu ievadītajiem skaitļiem ir: " + smallerNumber);
+
+Console.WriteLine("5.uzdevums: Iegūt skaitļu dalījuma atlikumu izmantojot - %");
+
+Console.WriteLine("Ievadiet dalāmo skaitli");
+string dalāmais = Console.ReadLine();
+int dalāmaisUser = int.Parse(dalāmais);
+Console.WriteLine("Ievadiet dalītāju");
+string dalītāju = Console.ReadLine();
+int dalītājsUser = int.Parse(dalītāju);
+int dalījums = dalāmaisUser / dalītājsUser;
+int atlikums = dalāmaisUser % dalītājsUser;
+
+Console.WriteLine("Dalījuma atlikums ir :" + atlikums);
+
+Console.WriteLine("6. uzdevums: Parādīt vai lietotājs ievadījis pāra vai nepāra skaitli");
+
+Console.WriteLine("Ievadiet skaitli");
+string num = Console.ReadLine();
+int numUser = int.Parse(num);
+int num1 = 2;
+int divided = numUser / num1;
+int divisionLeft = numUser % num1;
+bool pāraSkaitlis = divisionLeft <= 0;
+bool nepāraskaitlis = divisionLeft > 0;
+bool vai = pāraSkaitlis || !nepāraskaitlis;
+
+Console.WriteLine("Jūsu ievadītais skaitlis ir pāra skaitlis:  " + vai);
+
+
+Console.WriteLine("7. uzdevums: Iegūt no lietotāja taisnstūra malu vērtības un aprēķināt taisnstūra laukumu ");
+
+Console.WriteLine("Ievadiet taisnstūra agšējās malas garumu");
+string mala1 = Console.ReadLine();
+double mala1User = double.Parse(mala1);
+Console.WriteLine("Ievadiet taisnstūra sānu malas garumu");
+string mala2 = Console.ReadLine();
+double mala2User = double.Parse(mala2);
+double laukums = mala1User * mala2User;
+double laukums2 = Math.Round(laukums, 2);
+
+Console.WriteLine("Taisnstūra laukums ir: " + laukums2);
+//Console.WriteLine(laukums);
+
+Console.WriteLine("8.uzdevums: Iegūt no lietotāja skaitli, lai aprēķinātu taisnleņķa vienādsānu trijstūra laukumu ");
+
+//Console.WriteLine("Ievadiet trijstūra pirmās katetes garumu");
+//string katete1 = Console.ReadLine();
+//double katete1User = double.Parse(katete1);
+//Console.WriteLine("Ievadiet trijstūra otrās katetes garumu");
+//string katete2 = Console.ReadLine();
+//double katete2User = double.Parse(katete2);
+//double laukums = katete1User * katete2User;
+//double laukums2 = laukums / 2;
+
+//Console.WriteLine("Taisleņķa trijstūra laukums ir:  " + laukums2);
+
+Console.WriteLine("Ievadiet trijstūra malas garumu");
+string katete = Console.ReadLine();
+double kateteAbasUser = double.Parse(katete);
+double laukumsv = kateteAbasUser * kateteAbasUser;
+double laukumsv2 = laukumsv / 2;
+
+Console.WriteLine("Vienādsānu trijstūra laukums ir:  " + laukumsv2);
+
+Console.WriteLine("9. uzdevums: Iegūst lietotāja vecumu un izvada lietotājam vārdu un vecumu izmantojot interpolāciju");
+
+
+Console.WriteLine("Kāds ir Tavs vārds ?");
+string userName9 = Console.ReadLine();
+
+Console.WriteLine("Kāds ir Tavs vecums ?");
+string age9 = Console.ReadLine();
+int ageUser9 = int.Parse(age9);
+
+Console.WriteLine($"Sveiks, {userName9}, Tavs vecums ir {age9}. ");
+
+
---
+++ primitiv type demo/primitiv type demo/Program.cs
@@ -0,0 +1,185 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string mygreating = "Mans vārds ir Andris";
+string mygreeting1 = "es mācos C#";
+string mygreating2 = "Mans vecums ir";
+string mygreeting3 = "51";
+int age = 60;
+int age1 = 51;
+char komats = ',';
+Char punkts = '.';
+Char atstarpe = ' ';
+Double pi = 3.314;
+char favoriteSymbol = '@';
+bool isMale = true;
+
+    ;
+string atdalošāsvītra = "-------------------------------";
+
+Console.WriteLine(mygreating);
+Console.WriteLine(mygreeting1);
+Console.WriteLine($"{age},{mygreeting3}");
+Console.WriteLine(pi);
+Console.WriteLine(favoriteSymbol);
+Console.WriteLine(isMale);
+
+
+Console.WriteLine(atdalošāsvītra);
+Console.WriteLine();
+
+double a = 10;
+double b = 35;
+double sum = a + b;
+double differnce = b - a;
+double multiplication = a * b;
+double divided = b / a;
+
+int c = 5;
+int d = 2;
+
+int sum1 = c + d;
+int difference1 = c - d;
+int muliplication1 = c * d;
+double divided1 = c / (double)d;
+
+Console.WriteLine("pareizi jāeraksta int sum a+b, kāpēc");
+Console.WriteLine(sum);
+//Console.WriteLine(a + b);
+//Console.WriteLine(a - b);
+//Console.WriteLine(b - a);
+Console.WriteLine(differnce);
+Console.WriteLine(multiplication);
+Console.WriteLine(divided);
+
+Console.WriteLine($"{divided1} , {difference1} , {muliplication1} , {(double)a / b}");
+
+
+Console.WriteLine();
+Console.WriteLine(atdalošāsvītra);
+Console.WriteLine();
+
+int divisionLeft = c % d;
+int divided2 = c / d;
+Console.WriteLine(divided2);
+Console.WriteLine("dalījums");
+Console.WriteLine(divisionLeft);
+Console.WriteLine("atlikums");
+// 1) 5 / 2 = 2,5
+// 2) 2 * 2 = 4
+// 3) 5 - 4 = 1(atbilde) ja vēlas precīzu dalījumu aiz komata, tad jāeksperimentē ar double
+
+
+Console.WriteLine();
+Console.WriteLine(atdalošāsvītra);
+Console.WriteLine();
+
+c++;
+d--;
+
+Console.WriteLine(c);
+Console.WriteLine(d);
+
+Console.WriteLine();
+Console.WriteLine(atdalošāsvītra);
+Console.WriteLine();
+
+int num1 = 10;
+int num2 = 15;  
+
+bool isEqual = num1 == num2;
+bool isnotEqual = num2 != num1;  
+bool isLarger = (num1 > num2);
+bool isSmaller = (num1 < num2);
+bool isLargerEqual = (num1 >= num2);
+bool isSmallerEqual = (num1 <= num2);
+
+Console.WriteLine(isEqual);
+Console.WriteLine(isnotEqual);
+Console.WriteLine(isLarger);
+Console.WriteLine(isSmaller);
+Console.WriteLine(isLargerEqual);
+Console.WriteLine(isSmallerEqual);
+
+Console.WriteLine();
+Console.WriteLine(atdalošāsvītra);
+Console.WriteLine();
+
+bool isTrue = true;
+bool isFalse = false;
+
+bool andAction = isTrue && isFalse;  //ir patiess, ja abi ir true
+bool orAction = isTrue || isFalse;    //ir patiess, ja kaut viens ir true
+bool convertedTrue = !isTrue;            //apvērš mainīgā vērtību uz pretējo
+bool convertedTrue1 = !isFalse;
+
+Console.WriteLine(andAction);
+Console.WriteLine(orAction);
+Console.WriteLine(convertedTrue);
+Console.WriteLine(convertedTrue1);
+
+Console.WriteLine();
+Console.WriteLine(atdalošāsvītra);
+Console.WriteLine();
+
+int x = 4;
+
+x += 2;     //x = x + 2
+Console.WriteLine(x);
+
+x -= 3;     // x = x - 2
+Console.WriteLine(x);
+
+x *= 3;
+Console.WriteLine(x);
+x /= 3;
+Console.WriteLine(x);
+
+Console.WriteLine();
+Console.WriteLine(atdalošāsvītra);
+Console.WriteLine();
+
+//// Dabut input no lietotāja
+
+//// 1. Paprasam lietotājam ievadīt viņavārdu
+//Console.WriteLine("Lūdzu ievadi savu vārdu ?");
+
+//// 2. Sagaidam lietotāja ievadi 
+//string userName = Console.ReadLine();
+
+//// 3. Sasveicinamies ar lietotāju izmantojot viņa vārdu paterns: "Sveiks: {vārds}"
+//Console.WriteLine("Sveiks: " + userName);
+
+//// Skaitļa ievade
+
+//Console.WriteLine("Lūdzu ievadi skaitli ?");
+
+//string userNumberString = Console.ReadLine();
+//int userNumber = int.Parse(userNumberString);
+
+//int byThenLarger = userNumber + 10;
+
+//Console.WriteLine("Tavs skaitlis palielināts par 10: " + byThenLarger);
+
+int positiveNumber = 15;
+int negativeNumber = -15;
+
+int positiveAbs = Math.Abs(positiveNumber);
+int negativeAbs = Math.Abs(negativeNumber);
+
+Console.WriteLine("Default negative: " + negativeNumber);
+Console.WriteLine(positiveAbs);
+Console.WriteLine(negativeAbs);
+
+int maxNumberHigh = 40;
+int maxNumberLow = 29;
+
+int largerNumber = Math.Max(maxNumberHigh, maxNumberLow);
+Console.WriteLine("Larger number: " + largerNumber);
+
+int maxNumberHigh = 40;
+int maxNumberLow = 29;
+
+int largerNumber = Math.Max(maxNumberHigh, maxNumberLow);
+
+
+
